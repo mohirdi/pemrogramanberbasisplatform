ef56b944bf3e54a8f6b38c218d6adad94f61ca5e
mohirdi <114629894+mohirdi@users.noreply.github.com>
2023-10-04 13:38:16 +0700

    Add files via upload

---
+++ pbpsesi2.js
@@ -0,0 +1,8 @@
+let namaDepan = "Moh";
+let namaBelakang = " Irdi";
+let umur = 20;
+let status = true ;
+
+console.log ("Introduce my self, my name is "+ namaDepan + namaBelakang)
+console.log (" My age now "+ umur +" Years old");
+console.log ("im now not yet married "+ status) ;
